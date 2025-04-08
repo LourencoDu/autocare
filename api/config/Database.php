e861da191acaf38e36cf634413ec3b4dbeaaafc1
Eduardo <lourenco.e@pucpr.edu.br>
2025-04-08 19:57:51 -0300

    banco

--- api/config/Database.php
+++ api/config/Database.php
@@ -12,7 +12,6 @@ class Database {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name",
                                   $this->username, $this->password);
             $this->conn->exec("set names utf8");
-            echo "conectou";
         } catch(PDOException $exception) {
             echo "Erro de conexão: " . $exception->getMessage();
         }
