bc2595482c61a57174ff37867ebf45e09ceb0387
Vinicius Veiga <vinicius.veigasantana@gmail.com>
2025-04-27 15:41:07 -0300

    ajustes veiculo para novo padrão

--- Controller/VeiculoController.php
+++ Controller/VeiculoController.php
@@ -14,13 +14,19 @@ final class VeiculoController extends Controller {
     $this->render();
   }
 
+  private function backToIndex(): void {
+    Header("Location: /".BASE_DIR_NAME."/veiculo");
+  }
+
+
   public static function cadastrar() : void {
     parent::isProtected();
 
     $model = new Veiculo();
-    $model->ano = 2012;
-    $model->apelido = "Meu Carro";
-    $model->id_modelo_veiculo = 1;
+    $model->ano = $_POST["ano"];;
+    $model->apelido = $_POST["apelido"];
+    $model->id_usuario = $_POST["id_usuario"];
+    $model->id_modelo_veiculo = $_POST["id_modelo_veiculo"];
 
 
     $model->save();
@@ -37,5 +43,104 @@ final class VeiculoController extends Controller {
     ];
 
     $this->index();
+  }
+
+  public function atualizar(): void
+  {
+    parent::isProtected();
+
+    $this->view = "Crud/form.php";
+    $this->titulo = "Atualizar Veiculo";
+
+    $this->data = [
+      "fields" => [
+        "ano" => ["name" => "ano", "label" => "ano", "type" => "int", "required" => true],
+        "apelido" => ["name" => "apelido", "label" => "apelido", "type" => "text", "required" => true],
+        "id_modelo_veiculo" => ["name" => "id_modelo_veiculo", "label" => "id_modelo_veiculo", "type" => "int", "required" => true]
+      ]
+    ];
+
+    $model = new Veiculo();
+    $id = isset($_GET["id"]) ? $_GET["id"] : null;
+
+    if($id != null) {
+      $model = $model->getById((int) $id);
+
+      if($model != null) {
+        $this->data["form"] = [
+          "ano" => $model->ano,
+          "apelido" => $model->apelido,
+          "id_modelo_veiculo" => $model->id_modelo_veiculo
+        ];
+    
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+          try {           
+            $model->ano = $_POST["ano"];
+            $model->apelido = $_POST["apelido"];
+            $model->id_modelo_veiculo = $_POST["id_modelo_veiculo"];
+
+            var_dump($model);
+    
+            $model->save();
+    
+            $this->backToIndex();
+          } catch (\Throwable $th) {
+            $this->data = array_merge($this->data, [
+              "erro" => "Falha ao atualizar registro. Erro: ".$th->getMessage(),
+              "exception" => $th->getMessage()
+            ]);
+          }
+        }
+    
+        $this->render();
+      }  else {
+        $this->backToIndex();
+      }    
+    } else {
+      $this->backToIndex();
+    }
+  }
+
+  public function deletar(): void
+  {
+    parent::isProtected();
+
+    $this->view = "Crud/deletar.php";
+    $this->titulo = "Deletar Prestador";
+
+    $model = new Veiculo();
+    $id = isset($_GET["id"]) ? $_GET["id"] : null;
+
+    if($id != null) {
+      $model = $model->getById((int) $id);
+
+      if($model != null) {
+        $this->data["infos"] = [
+          "id" => $model->id,
+          "ano" => $model->ano,
+          "apelido" => $model->apelido,
+          "id_usuario" => $model->id_usuario,
+          "id_modelo_veiculo" => $model->id_modelo_veiculo
+        ];
+    
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+          try {           
+            Veiculo::delete((int) $id);
+            $this->backToIndex();
+          } catch (\Throwable $th) {
+            $this->data = array_merge($this->data, [
+              "erro" => "Falha ao atualizar registro. Erro: ".$th->getMessage(),
+              "exception" => $th->getMessage()
+            ]);
+          }
+        }
+    
+        $this->render();
+      }  else {
+        $this->backToIndex();
+      }    
+    } else {
+      $this->backToIndex();
+    }
   }
 }
--- Model/Veiculo.php
+++ Model/Veiculo.php
@@ -7,24 +7,24 @@ use AutoCare\DAO\VeiculoDAO;
 final class Veiculo {
   public $id, $ano, $apelido, $id_usuario, $id_modelo_veiculo;
   
-  function getById(int $id) : ?Veiculo {
+  public static function getById(int $id) : ?Veiculo {
     return (new VeiculoDAO())->selectById($id);
   }
 
-  function getAllByLoggedUser() : array {
+  public static function getAllByLoggedUser() : array {
     $id_usuario = $_SESSION["usuario"]["id"];
     return (new VeiculoDAO())->selectByUser($id_usuario);
   }
 
-  function getAllRows() : array {
+  public static function getAllRows() : array {
     return (new VeiculoDAO())->select();
   }
 
-  function save() : Veiculo {
+  public function save() : Veiculo {
     return (new VeiculoDAO())->save($this);
   }
 
-  function delete(int $id) : bool {
+  public static function delete(int $id) : bool {
     return (new VeiculoDAO())->delete($id);
   }
 }
