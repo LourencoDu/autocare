08e128fb423b5b95bcd4b2909660a4245f8651bb
Eduardo <eduardo.silva@hubchain.io>
2025-04-27 22:04:32 -0300

    crud de veiculo

--- Controller/VeiculoController.php
+++ Controller/VeiculoController.php
@@ -18,31 +18,67 @@ final class VeiculoController extends Controller {
     Header("Location: /".BASE_DIR_NAME."/veiculo");
   }
 
-
-  public static function cadastrar() : void {
+  public function listar(): void
+  {
     parent::isProtected();
 
     $model = new Veiculo();
-    $model->ano = $_POST["ano"];;
-    $model->apelido = $_POST["apelido"];
-    $model->id_usuario = $_POST["id_usuario"];
-    $model->id_modelo_veiculo = $_POST["id_modelo_veiculo"];
 
+    $lista = $model->getAllRows();
 
-    $model->save();
-    echo "Veiculo cadastrado com sucesso!";
-  }
-
-  public function listar() : void {
-    parent::isProtected();
-
-    $model = new Veiculo();
+    $baseDirName = BASE_DIR_NAME;
 
     $this->data = [
-      "lista" => $model->getAllByLoggedUser()
+      "lista" => $lista,
+      "addLink" => "/$baseDirName/veiculo/cadastrar",
+      "editLink" => "/$baseDirName/veiculo/atualizar",
+      "deleteLink" => "/$baseDirName/veiculo/deletar",
     ];
 
     $this->index();
+  }
+
+  public function cadastrar(): void
+  {
+    parent::isProtected();
+
+    $this->view = "Crud/form.php";
+    $this->titulo = "Novo Prestador";
+
+    $this->data = [
+      "fields" => [
+        "ano" => ["name" => "ano", "label" => "Ano", "type" => "number", "required" => true],
+        "apelido" => ["name" => "apelido", "label" => "Apelido", "type" => "text", "required" => true],
+        "id_modelo_veiculo" => ["name" => "id_modelo_veiculo", "label" => "Modelo Veiculo", "type" => "text", "required" => true]
+      ]
+    ];
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+      try {
+        $model = new Veiculo();
+
+        $model->ano = $_POST["ano"];
+        $model->apelido = $_POST["apelido"];
+        $model->id_modelo_veiculo = $_POST["id_modelo_veiculo"];
+
+        $this->data["form"] = [
+          "ano" => $model->ano,
+          "apelido" => $model->apelido,
+          "id_modelo_veiculo" => $model->id_modelo_veiculo
+        ];
+
+        $model->save();
+
+        $this->backToIndex();
+      } catch (\Throwable $th) {
+        $this->data = array_merge($this->data, [
+          "erro" => "Falha ao adicionar registro. Erro: ".$th->getMessage(),
+          "exception" => $th->getMessage()
+        ]);
+      }
+    }
+
+    $this->render();
   }
 
   public function atualizar(): void
@@ -50,13 +86,13 @@ final class VeiculoController extends Controller {
     parent::isProtected();
 
     $this->view = "Crud/form.php";
-    $this->titulo = "Atualizar Veiculo";
+    $this->titulo = "Atualizar Veículo";
 
     $this->data = [
       "fields" => [
-        "ano" => ["name" => "ano", "label" => "ano", "type" => "int", "required" => true],
-        "apelido" => ["name" => "apelido", "label" => "apelido", "type" => "text", "required" => true],
-        "id_modelo_veiculo" => ["name" => "id_modelo_veiculo", "label" => "id_modelo_veiculo", "type" => "int", "required" => true]
+        "ano" => ["name" => "ano", "label" => "Ano", "type" => "number", "required" => true],
+        "apelido" => ["name" => "apelido", "label" => "Apelido", "type" => "text", "required" => true],
+        "id_modelo_veiculo" => ["name" => "id_modelo_veiculo", "label" => "Modelo Veiculo", "type" => "text", "required" => true]
       ]
     ];
 
@@ -78,8 +114,6 @@ final class VeiculoController extends Controller {
             $model->ano = $_POST["ano"];
             $model->apelido = $_POST["apelido"];
             $model->id_modelo_veiculo = $_POST["id_modelo_veiculo"];
-
-            var_dump($model);
     
             $model->save();
     
@@ -106,7 +140,7 @@ final class VeiculoController extends Controller {
     parent::isProtected();
 
     $this->view = "Crud/deletar.php";
-    $this->titulo = "Deletar Prestador";
+    $this->titulo = "Deletar Veículo";
 
     $model = new Veiculo();
     $id = isset($_GET["id"]) ? $_GET["id"] : null;
@@ -119,8 +153,7 @@ final class VeiculoController extends Controller {
           "id" => $model->id,
           "ano" => $model->ano,
           "apelido" => $model->apelido,
-          "id_usuario" => $model->id_usuario,
-          "id_modelo_veiculo" => $model->id_modelo_veiculo
+          "id_modelo_veiculo" => $model->id_modelo_veiculo,
         ];
     
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
