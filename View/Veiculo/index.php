53c40fa6902091f48617603eb4e49e86ef895e8f
Eduardo <lourenco.e@pucpr.edu.br>
2025-05-08 22:09:15 -0300

    add - veiculo - infos

--- View/Veiculo/index.php
+++ View/Veiculo/index.php
@@ -23,13 +23,34 @@ $quantidade = count($veiculos);
 
   <div class="flex flex-col gap-5 pt-5">
     <?php foreach ($veiculos as $index => $veiculo) : ?>
-      <div class="flex flex-row p-8 border border-gray-300 rounded-xl">
-        <div class="flex flex-row items-center justify-center h-12 w-12 border border-gray-300 rounded-4xl">
-          <i class="fa-solid fa-car"></i>
+      <div class="flex flex-row flex-wrap justify-between p-8 border border-gray-300 rounded-xl gap-4">
+        <div class="flex flex-row">
+          <div class="flex flex-row items-center justify-center h-12 w-12 border border-gray-300 rounded-4xl">
+            <i class="fa-solid fa-car"></i>
+          </div>
+          <div class="flex flex-col pl-3">
+            <span class="text-lg font-semibold"><?= $veiculo->apelido ?></span>
+            <span class="text-sm"><?= $veiculo->fabricante . " - " . $veiculo->modelo . " - " . $veiculo->ano ?></span>
+          </div>
         </div>
-        <div class="flex flex-col pl-3">
-          <span class="text-lg font-semibold"><?= $veiculo->apelido ?></span>
-          <span class="text-sm"><?= $veiculo->fabricante . " - " .$veiculo->modelo . " - " . $veiculo->ano ?></span>
+
+        <div class="flex flex-row">
+          <div class="flex flex-row gap-4">
+            <div class="flex flex-col border border-dashed border-gray-300 rounded-xl px-2 py-1 min-w-24 max-w-auto gap-1">
+              <span class="text-sm font-semibold"><?= $veiculo->fabricante ?></span>
+              <span class="text-xs text-gray-700">Fabricante</span>
+            </div>
+
+            <div class="flex flex-col border border-dashed border-gray-300 rounded-xl px-2 py-1 min-w-24 max-w-auto gap-1">
+              <span class="text-sm font-semibold"><?= $veiculo->modelo ?></span>
+              <span class="text-xs text-gray-700">Modelo</span>
+            </div>
+
+            <div class="flex flex-col border border-dashed border-gray-300 rounded-xl px-2 py-1 min-w-24 max-w-auto gap-1">
+              <span class="text-sm font-semibold"><?= $veiculo->ano ?></span>
+              <span class="text-xs text-gray-700">Ano</span>
+            </div>
+          </div>
         </div>
       </div>
     <?php endforeach; ?>
