87afaf1a9c5258e117200bd63111c35296fdcb90
Eduardo <eduardo.silva@hubchain.io>
2025-05-13 00:22:12 -0300

    [add] inicial - prestadores

--- View/Layout/logged.php
+++ View/Layout/logged.php
@@ -1,20 +1,24 @@
-<div class="flex flex-col flex-1 max-h-screen max-w-screen p-5 gap-4">
-  <?php include COMPONENTS . "header.php"; ?>
+<div class="flex flex-1 grow">
+  <div class="flex flex-col flex-1 max-h-screen max-w-screen p-5 gap-4">
+    <?php include COMPONENTS . "header.php"; ?>
 
-  <div class="flex flex-row flex-1 gap-4">
-    <?php include COMPONENTS . "sidemenu.php"; ?>
+    <div class="flex flex-row grow flex-1 gap-4 overflow-y-auto pr-1">
+      <?php include COMPONENTS . "sidemenu.php"; ?>
 
-    <div class="flex flex-col flex-1 border border-gray-700/20 rounded-xl p-5 px-24 bg-gray-50">
-      <div class="flex flex-row pb-5">
-      <div class="flex flex-row items-center gap-4 h-10">
-        <span class="font-medium text-lg">
-          <?= htmlspecialchars($titulo ?? 'Sem Título') ?>
-        </span>
-        <?php include COMPONENTS . "breadcrumbs.php"; ?>
+      <div class="flex flex-col grow border border-gray-700/20 rounded-xl px-[0.15rem] py-[0.15rem] bg-gray-50">
+        <div class="flex flex-col grow p-5 px-24 overflow-y-auto">
+          <div class="flex flex-row pb-5">
+            <div class="flex flex-row items-center gap-4 h-10">
+              <span class="font-medium text-lg">
+                <?= htmlspecialchars($titulo ?? 'Sem Título') ?>
+              </span>
+              <?php include COMPONENTS . "breadcrumbs.php"; ?>
+            </div>
+          </div>
+
+          <?php include VIEWS . $view; ?>
+        </div>
       </div>
-      </div>
-
-      <?php include VIEWS . $view; ?>
     </div>
   </div>
 </div>
