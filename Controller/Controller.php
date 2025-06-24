ef6855e6beba04e0e87c55dd47fba9ca31018790
Eduardo <eduardo.silva@hubchain.io>
2025-06-24 13:42:54 -0300

    fixing deploy

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -35,7 +35,8 @@ abstract class Controller
       "data" => $this->data,
     ];
     extract($config);
-    require_once VIEWS . 'Layout/index.php';
+    var_dump($config);
+    //require_once VIEWS . 'Layout/index.php';
   }
 
   final protected static function isProtected(?array $tiposBloqueados = null, ?array $tiposPermitidos = null, ?bool $json = false)
