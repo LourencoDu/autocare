1798a6ad03bacff2853e26503803dbaff9f5572b
Eduardo <eduardo.silva@hubchain.io>
2025-06-24 14:42:28 -0300

    fixing deploy

--- config.php
+++ config.php
@@ -19,10 +19,10 @@ function loadEnv($path)
 
 loadEnv(__DIR__ . '/.env');
 
-define("BASE_DIR", dirname(__FILE__, 1));
+define("BASE_DIR", dirname(__FILE__));
 define("BASE_DIR_NAME", "");
 define("PUBLIC_DIR", BASE_DIR_NAME."/public");
-
+echo BASE_DIR.($_ENV['DIR_VIEWS'] ?? "/View/");
 define("VIEWS", BASE_DIR.($_ENV['DIR_VIEWS'] ?? "/View/"));
 define("COMPONENTS", BASE_DIR.($_ENV['DIR_COMPONENTS'] ?? "/components/"));
 
