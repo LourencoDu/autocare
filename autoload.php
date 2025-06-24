be2cf2d195589ab0d06cd20778cc66c211000625
Eduardo <eduardo.silva@hubchain.io>
2025-06-24 13:20:38 -0300

    1

--- autoload.php
+++ autoload.php
@@ -1,16 +1,22 @@
 <?php
 
 spl_autoload_register(function ($nome_da_classe) {
-  // Remove barra invertida inicial, se existir
-  $nome_da_classe = ltrim($nome_da_classe, '\\');
-  
-  // Converte namespace para caminho (troca \ por /)
-  $arquivo = BASE_DIR . '/' . str_replace('\\', '/', $nome_da_classe) . '.php';
+    $prefixo = 'AutoCare\\';
 
-  if (file_exists($arquivo)) {
-    include $arquivo;
-  } else {
-    echo var_dump($arquivo);
-    throw new Exception("Arquivo não encontrado");
-  }
+    // Verifica se a classe começa com o prefixo
+    if (strpos($nome_da_classe, $prefixo) === 0) {
+        // Remove o prefixo
+        $nome_da_classe = substr($nome_da_classe, strlen($prefixo));
+    }
+
+    $caminho = str_replace('\\', '/', ltrim($nome_da_classe, '\\'));
+    $arquivo = rtrim(BASE_DIR, '/') . '/' . $caminho . '.php';
+
+    if (file_exists($arquivo)) {
+        include $arquivo;
+    } else {
+        echo "Tentando carregar: $arquivo\n";
+        throw new Exception("Arquivo não encontrado");
+    }
 });
+
